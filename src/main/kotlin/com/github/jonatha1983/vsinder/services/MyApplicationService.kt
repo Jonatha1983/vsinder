e21131bf025bd8beff4119b59c82ad200a4b03a8
GitHub Action <action@github.com>
2020-12-20 08:07:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/jonatha1983/vsinder/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jonatha1983/vsinder/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jonatha1983.vsinder.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jonatha1983.vsinder.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/jonatha1983/vsinder/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jonatha1983/vsinder/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.jonatha1983.vsinder.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jonatha1983.vsinder.MyBundle
 
 class MyProjectService(project: Project) {
 
